e27b736dd86c3835c6fd4aab414f8afa9629b513
Corey Post <corey.post0120@gmail.com>
2020-09-20 02:34:14 -0600

    initial commit/basic structure

---
+++ routes/api.js
@@ -0,0 +1,10 @@
+const router = require("./html")
+
+const router = require("express").Router();
+const Workout = require("../models/workout.js");
+
+router.post("/api/workouts", (req,res)=>{
+
+});
+
+module.exports = router;
---
+++ routes/html.js
@@ -0,0 +1,6 @@
+const router = require("express").Router();
+const path = require("path");
+
+//code
+
+module.exports = router;
